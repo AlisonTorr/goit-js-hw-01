e771a73091f4c7bc52d06ac33d524956e282fcd0
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2020-12-03 17:16:08 +0300

    Add files via upload

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+const ADMIN_PASSWORD = "jqueryismyjam";
+let message;
+
+const enterPassword = confirm("Введите пароль");
+console.log(enterPassword);
+
+const userPassword = prompt("Введите свой пароль");
+console.log(userPassword === ADMIN_PASSWORD);
+
+if (userPassword === ADMIN_PASSWORD) {
+  message = "Добро пожаловать!";
+} else if (userPassword !== ADMIN_PASSWORD) {
+  message = "Доступ запрещен, неверный пароль!";
+} else if (userPassword === null) {
+  message = "Отменено пользователем!";
+}
+
+alert(message);
