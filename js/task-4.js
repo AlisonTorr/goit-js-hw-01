8744ad36cb4bd03e1d82c2e390a458523e703cc2
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2020-12-03 17:40:33 +0300

    Add files via upload

---
+++ js/task-4.js
@@ -0,0 +1,18 @@
+const credits = 23580;
+const pricePerDroid = 3000;
+
+const purchasedDroidNumber = prompt("Введите количество дроидов к покупке");
+let message;
+let totalPrice = pricePerDroid * purchasedDroidNumber;
+
+if ((purchasedDroidNumber = null)) {
+  message = "Отменено пользователем!";
+} else if (purchasedDroidNumber > 0) {
+  message = totalPrice;
+}
+
+if (credits < totalPrice) {
+  message = "Недостаточно средств на счету!";
+} else {
+  message = `Вы купили $purchsedDroidNumber дроидов, на счету осталось ($credits - $totalPrice) кредитов.`;
+}
