feb6d165499efb925d1b313b14e18387d71ca547
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-01-13 14:43:53 +0300

    Add files via upload

--- js/task-5.js
+++ js/task-5.js
@@ -8,44 +8,48 @@ let message;
 const enterCountry = prompt("Введите страну доставки");
 console.log(enterCountry);
 
-switch (enterCountry.toLowerCase()) {
-  case "австралия": {
-    country = Австралию;
-    deliveryPrice = 170;
-    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
-    break;
-  }
+if (enterCountry === null) {
+  break;
+} else {
+  switch (enterCountry.toLowerCase()) {
+    case "австралия": {
+      country = Австралию;
+      deliveryPrice = 170;
+      message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
+      break;
+    }
 
-  case "китай": {
-    country = Китай;
-    deliveryPrice = 100;
-    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
-    break;
-  }
+    case "китай": {
+      country = Китай;
+      deliveryPrice = 100;
+      message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
+      break;
+    }
 
-  case "чили": {
-    country = Чили;
-    deliveryPrice = 250;
-    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
-    break;
-  }
+    case "чили": {
+      country = Чили;
+      deliveryPrice = 250;
+      message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
+      break;
+    }
 
-  case "индия": {
-    country = Индию;
-    deliveryPrice = 80;
-    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
-    break;
-  }
+    case "индия": {
+      country = Индию;
+      deliveryPrice = 80;
+      message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
+      break;
+    }
 
-  case "ямайка": {
-    country = Ямайку;
-    deliveryPrice = 120;
-    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
-    break;
-  }
+    case "ямайка": {
+      country = Ямайку;
+      deliveryPrice = 120;
+      message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
+      break;
+    }
 
-  default:
-    message = "В вашей стране доставка не доступна";
+    default:
+      message = "В вашей стране доставка не доступна";
+  }
 }
 
 alert(message);
